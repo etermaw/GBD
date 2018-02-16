c9751fa1ac13696c5cb879f356905e1aa13475d6
etermaw <etermaw@gmail.com>
2018-02-16 19:00:30 +0100

    added HL dispatch aborting

--- dispatcher.py
+++ dispatcher.py
@@ -1,8 +1,14 @@
 A_MODS = {0xA, 0x1A, 0x2A, 0x3A, 0xF0, 0xF1, 0xF2, 0xC6, 0xD6, 0xE6, 0xF6, 0xFA, 0xCE, 0xDE, 0xEE} | \
-         set(range(0x77, 0xB8)) | set([0xCB07 + x*0x10 for x in range(0, 0x10)]) | \
+         set(range(0x77, 0xB8)) | \
+         set([0xCB07 + x*0x10 for x in range(0, 0x10)]) | \
          set([0xCB0F + x*0x10 for x in range(0, 0x10)])
 
-HL_MODS = {}
+HL_MODS = set(range(0x60, 0x70)) | \
+          set([0xCB05 + i*0x10 for i in range(0, 0x10)]) | \
+          set([0xCB06 + i*0x10 for i in range(0, 0x10)]) | \
+          set([0xCB0C + i*0x10 for i in range(0, 0x10)]) | \
+          set([0xCB0D + i*0x10 for i in range(0, 0x10)]) | \
+          {0x09, 0x19, 0x29, 0x39, 0xE1, 0xF8}
 
 
 def mbc_mapper(bank_num):
@@ -50,5 +56,9 @@ def get_hl_mod(opcode_list):
 
             if hval is not None:
                 return (hval << 8) | lval
+
+        elif op.opcode in HL_MODS:
+            op.warning = 'HL resolving aborted here'
+            break
 
     raise Exception('Could not resolve HL value!')
