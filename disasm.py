976d3d7c4dcf7e6b177af86129efa0306e475b7c
etermaw <etermaw@gmail.com>
2018-02-16 14:17:42 +0100

    added handling when file is not found

--- disasm.py
+++ disasm.py
@@ -11,14 +11,18 @@ def main():
         start_bank = int(sys.argv[3], 16) if len(sys.argv) >= 4 else 1
         depth = int(sys.argv[4]) if len(sys.argv) == 5 else 999999999
 
-        with open(file_name, 'rb') as file:
-            binary = file.read()
+        try:
+            with open(file_name, 'rb') as file:
+                binary = file.read()
 
-        deasm = TraceFollower(binary)
-        deasm.trace_all_paths(start_pc, start_bank, depth)
+            deasm = TraceFollower(binary)
+            deasm.trace_all_paths(start_pc, start_bank, depth)
 
-        for i in deasm.chunk_cache:
-            print_opcodes(deasm.chunk_cache[i])
+            for i in deasm.chunk_cache:
+                print_opcodes(deasm.chunk_cache[i])
+
+        except FileNotFoundError:
+            print("ERROR: File not found!")
 
     else:
         print("Args: <program name> <start pc [hex, default: 0x100]> <ROM bank [hex, default 1]> <depth [default: inf]>")
